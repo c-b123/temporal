f33fec9b4ba3ce9b3cd455c9d21ea65e74445a52
Christian <christian.berger2@uzh.ch>
2024-06-17 16:25:21 +0200

    Init changed

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author='chris',
     url='https://github.com/c-b123/temporal',
     author_email='christian.berger2@uzh.ch',
-    python_requires=">=3.11",
+    python_requires=">=3.6",
     packages=find_packages()
 )
